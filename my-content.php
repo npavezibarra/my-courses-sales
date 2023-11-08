02b0406ac75975474b09d5f1ac1a01496cd87690
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2023-11-08 15:57:51 -0300

    Total Earnings
    Agregué "Total Earnings" calculando el total de todas las ordenes pertenecientes al usuario logeado. Están dentro de un div con id "instructor-earnings".
    
    Adicionalmente agregué el id 'tablaOrdenes' a la tabla que muestra los datos.

---
+++ my-content.php
@@ -0,0 +1,81 @@
+<?php
+
+function my_courses_sales_content() {
+    // Check if user is logged in
+    if (is_user_logged_in()) {
+        // Get the current user's ID
+        $current_user_id = get_current_user_id();
+
+        // Initialize total earnings variable
+        $total_earnings = 0;
+
+        // Get all completed orders
+        $customer_orders = wc_get_orders(array(
+            'status' => 'completed',
+        ));
+
+        if ($customer_orders) {
+            // Calculate total earnings first
+            foreach ($customer_orders as $order) {
+                foreach ($order->get_items() as $item_id => $item) {
+                    // Retrieve the '_course_instructor_id' value from the product
+                    $product_id = $item->get_product_id();
+                    $course_instructor_id = get_post_meta($product_id, '_course_instructor_id', true);
+                    
+                    // Check if the current user's ID matches the 'course_instructor' ID
+                    if ($current_user_id == $course_instructor_id) {
+                        // Update total earnings
+                        $total_earnings += $item->get_total();
+                    }
+                }
+            }
+
+            echo "<h2>" . __('Your Course Sales', 'text-domain') . "</h2>";
+
+            // Display the total earnings above the table
+            echo '<div id="instructor-earnings">';
+            echo '<strong>Total Earnings: ' . wc_price($total_earnings) . '</strong>';
+            echo '</div>';
+
+            // Start the table
+            echo '<table id="tablaOrdenes">';
+            echo '<tr>';
+            echo '<th>' . __('Product Title', 'text-domain') . '</th>';
+            echo '<th>' . __('Course Instructor', 'text-domain') . '</th>';
+            echo '<th>' . __('Price', 'text-domain') . '</th>';
+            echo '<th>' . __('Date', 'text-domain') . '</th>';
+            echo '<th>' . __('First Name (Billing)', 'text-domain') . '</th>';
+            echo '</tr>';
+
+            foreach ($customer_orders as $order) {
+                foreach ($order->get_items() as $item_id => $item) {
+                    // Retrieve the '_course_instructor_id' value from the product
+                    $product_id = $item->get_product_id();
+                    $course_instructor_id = get_post_meta($product_id, '_course_instructor_id', true);
+                    
+                    // Get the user's data
+                    $course_instructor = get_userdata($course_instructor_id);
+                    $course_instructor_nickname = $course_instructor ? $course_instructor->display_name : '';
+
+                    // Check if the current user's ID matches the 'course_instructor' ID
+                    if ($current_user_id == $course_instructor_id) {
+                        echo '<tr>';
+                        echo '<td>' . $item->get_name() . '</td>';
+                        echo '<td>' . esc_html($course_instructor_nickname) . '</td>';
+                        echo '<td>' . wc_price($item->get_total()) . '</td>';
+                        echo '<td>' . wc_format_datetime($order->get_date_created()) . '</td>';
+                        echo '<td>' . $order->get_billing_first_name() . '</td>';
+                        echo '</tr>';
+                    }
+                }
+            }
+
+            // Close the table
+            echo '</table>';
+        } else {
+            echo '<p>' . __('No completed orders found.', 'text-domain') . '</p>';
+        }
+    } else {
+        echo '<p>' . __('Please log in to view your orders.', 'text-domain') . '</p>';
+    }
+}
